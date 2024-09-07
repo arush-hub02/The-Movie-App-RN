1d1d70e7ff915e195b6234a41f4dafba77877c46
Arush <arushdeepguptacse@gmail.com>
2024-09-07 17:11:28 +0530

    Movies Header added with icons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Bars3CenterLeftIcon, MagnifyingGlassIcon } from "react-native-heroicons/outline";
+import { styles } from "../theme";
+import TrendingMovies from "../components/trendingMovies";
+
+
 
 export default function HomeScreen() {
+    const [trending, setTrending] = useState([1,2,3])
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
-  )
+    <View className="flex-1 bg-neutral-800 pt-2">
+      {/* searchBar and logo */}
+      <SafeAreaView className="mb-3">
+        <View className="flex-row justify-between items-center mx-5">
+            <Bars3CenterLeftIcon  size="30" strokeWidth={2} color="white"/>
+            <Text className="text-3xl font-bold text-white" >
+                <Text style={styles.text}>M</Text>ovies</Text>
+            <TouchableOpacity>
+                <MagnifyingGlassIcon size="30" strokeWidth={2} color="white"/>
+            </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+
+      <ScrollView 
+      showsVerticalScrollIndicator={false}
+      contentContainerStyle={{paddingBottom: 10 }}
+      >
+
+        {/* Trending movies carousel */}
+
+        <TrendingMovies data={trending}/>
+        
+      </ScrollView>
+    </View>
+  );
 }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'red',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
